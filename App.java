9592739f583f2a60d232411426117802f74f4824
pzhiheng <143123498+pzhiheng@users.noreply.github.com>
2024-06-14 21:30:40 -0700

    Update App.java

--- App.java
+++ App.java
@@ -1,14 +1,17 @@
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
 import java.util.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.*;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Object obj = new Object();
         ChromeDriver driver = new ChromeDriver();
         PrintWriter out = new PrintWriter("class.txt");
+        BufferedWriter bf = new BufferedWriter(out);
         ArrayList<WebElement> ListOfCourses = new ArrayList<>(); 
         driver.get("https://sis.nyu.edu/psc/csprod/EMPLOYEE/SA/c/NYU_SR.NYU_CLS_SRCH.GBL");
         //driver.get("https://quotes.toscrape.com/");
@@ -21,36 +24,80 @@ public class App {
             }*/
         //obj.wait(500);
         //driver.manage().wait(5000);
-        
-       try{
+        ArrayList<String> testing = new ArrayList<>();
+
+        String courseID = "win0divSELECT_COURSE_row$";
+        for (int i = 0 ; i <= 87 ; i ++){
+            try{
+                
+                courseID = courseID + String.valueOf(i);
+                WebElement Course = driver.findElement(By.id(courseID));
+                WebElement htmlArea = Course.findElement(By.className("ps_box-htmlarea"));
+                //coursename
+                String CourseName = htmlArea.findElement(By.xpath("./div/span/b")).getText();
+                //school
+                String school = Course.findElement(By.xpath("./div/div[2]/span")).getText();
+                //session
+                bf.write(CourseName + "," + school);
+                bf.newLine();
+                List<WebElement> list = new ArrayList<>();
+                list = Course.findElements(By.xpath("./div/div[3]/div/div/div/div[2]/div"));
+                for (WebElement session : list){
+                    ///html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div[7]/div[2]/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div[1]
+                    //first session xpath
+                    String time_date_professor = session.findElement(By.xpath("./div/div/div/table/tbody/tr/td/div[2]")).getText();
+                    String classNO = session.findElement(By.xpath("./div/div/div/table/tbody/tr/td/div[1]/b")).getText();
+                    String sessionNO = session.findElement(By.xpath("./div/div/div/table/tbody/tr/td/div[2]/div[1]")).getText();
+                    String status = session.findElement(By.xpath("./div/div/div/table/tbody/tr/td/div[2]/div[4]/span[2]")).getText();
+                    String instructionMode = session.findElement(By.xpath("./div/div/div/table/tbody/tr/td/div[2]/div[6]/span")).getText();
+                    String courseLocation = session.findElement(By.xpath("./div/div/div/table/tbody/tr/td/div[2]/div[7]")).getText();
+                    String component = session.findElement(By.xpath("./div/div/div/table/tbody/tr/td/div[2]/div[8]")).getText();
+                    bf.write(classNO+","+sessionNO+","+time_date_professor+","+status+","+instructionMode+","+courseLocation+","+component);
+                    bf.newLine();
+                    bf.newLine();
+
+                }
+                courseID = "win0divSELECT_COURSE_row$";
+                bf.newLine();
+                bf.newLine();
+                
+
+            }catch(Exception e){
+
+                System.out.println("Element not found or error occurred for index: " + i);
+            }
+        }
+
+
+       /*try{
        // List<WebElement> elements = driver.findElements(By.xpath("//div[@class='quote']/span[@class='text']"));
        //String str = driver.findElement(By.className("ps_box-value")).getText();
        String courseID = "win0divSELECT_COURSE_row$";
-       for (int i = 0 ; i < 86 ; i ++){
+       for (int i = 0 ; i <= 86 ; i ++){
         courseID = courseID + String.valueOf(i);
         ListOfCourses.add(driver.findElement(By.id(courseID)));
         System.out.println(courseID);
-        courseID = courseID.substring(0,25);}
+        courseID = courseID.substring(0,25);
+    }
         ArrayList<String> testing = new ArrayList<>();
 
         for (WebElement e: ListOfCourses){
-             String CourseName = e.findElement(By.xpath("//b")).getText();
+             String CourseName = e.findElement(By.xpath("./b")).getText();
              testing.add(CourseName);
 
         }
-        System.out.println(testing.toString());
-    
-       /*for(String str: list){
-        System.out.println(str);
-        out.println(str);
 
-       }*/}
+    
+
+
+       }
        catch (Exception e){
         System.out.println("hhhhh");
         System.out.println(e.toString());
 
-       }
+       }*/
         driver.quit();
 
     }
 }
+
