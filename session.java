e00f34df3580e4527974eb26937ce6979e3978e1
pzhiheng <143123498+pzhiheng@users.noreply.github.com>
2024-06-19 22:20:24 -0700

    Update session.java

--- session.java
+++ session.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class session {
 
 
@@ -12,6 +13,8 @@ public class session {
     private String time;
     private String room = "unknown";
     private boolean isLecture;
+    private ArrayList<session> resitations = new ArrayList<>();
+    
 
     public session(String classNo, String sessionNo, String status, String instructionMode, String courseLocation, String component, String other){
         this.classNo = classNo;
@@ -26,8 +29,16 @@ public class session {
         else{
             this.isLecture = false;
         }
-        
 
+    }
+    public String toString(){
+        String str = "";
+        str += "Session number: " + this.sessionNo + "\n";
+        str += "Instruction Mode: " + this.instructionMode + "\n";
+        str += "Course Location: " + this.courseLocation + "\n";
+        str += "Lecture: " + this.isLecture + "\n";
+    
+        return str;
 
     }
 
