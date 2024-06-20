f4beb001d59c8ee1a9f1028028ad18d7dca42b2a
pzhiheng <143123498+pzhiheng@users.noreply.github.com>
2024-06-19 22:20:44 -0700

    Update course.java

--- course.java
+++ course.java
@@ -3,14 +3,26 @@ public class course {
 
     public ArrayList<session> sessions = new ArrayList<>();
     public String name;
+    public String courseNo;
 
-    public course(String name){
+    public course(String name, String courseNo){
         this.name = name;
+        this.courseNo = courseNo;
         sessions = new ArrayList<>();
     }
 
     public boolean addSession(session s){
         return sessions.add(s);
+    }
+
+    public String toString(){
+        String str = "";
+        str += "Course name: " + this.name + "\n";
+        str += "Course number: " + this.courseNo + "\n";
+        for (session s : this.sessions  ){
+            str += s.toString();
+        }
+        return str;
     }
 
 }
